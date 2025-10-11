5a7d976fd9736bfcd1431a2e5de5fb9213ca9d9f
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-10-11 11:31:50 +0300

    Day01 | Part02

---
+++ Day01/01 - Lecture/Day01/Demo/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Display()
+        {
+            return $"Hello From Display() Inside TestController!";
+        }
+        public ViewResult ShowAddForm()
+        {
+            return View();
+        }
+        public int Add(int? x, int y)
+        {
+            return (x ?? 0) + y;
+        } 
+    }
+}
