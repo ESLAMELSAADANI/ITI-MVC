674775f0726d86e7fcf9153f9bf4b6d5df7b2c89
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-10-11 23:28:50 +0300

    Day01 | Part03 => Ways Of How Send Data From Controller To View [ViewData - ViewBag - Model - ViewModel - TempData]

---
+++ Day01/01 - Lecture/Day01/Demo/ViewModels/HomoginuousTypesVM.cs
@@ -0,0 +1,13 @@
+﻿using Demo.Models;
+
+namespace Demo.ViewModels
+{
+    public class HomoginuousTypesVM
+    {
+        public int x { set; get; }
+        public string name { set; get; }
+        public List<string> names { set; get; }
+        public Student student { set; get; }
+        public List<Student> students { set; get; }
+    }
+}
